1edfd1dd37fb1f2a5b9ee98a704577317ca7319a
Abbey Sparrow <abbey@khrome.net>
2021-04-06 21:22:01 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+pi-camera-shim
+==============
+Use a camera when local to pi, use test data when on a local system.
---
+++ camera.js
@@ -0,0 +1,42 @@
+var Camera = require("@zino-hofmann/pi-camera-connect");
+var path = require('path');
+
+var andFail = function(err){ throw err };
+var pos = 0; //this is exceptionally lazy and needs an attached reference
+//TODO: make it work for more than 1 stream at a time
+
+var loop = function(handler, backupPath){
+    if(true){ //TODO: detect if raspi, if not, deliver images from backup Path
+        var streamCamera = new Camera.StreamCamera({
+            codec: Camera.Codec.MJPEG,
+        });
+        streamCamera.startCapture().then(function(){
+            streamCamera.takeImage().then(function(image){
+                streamCamera.stopCapture().then(function(){
+                    setTimeout(function(){
+                        handler(image, function(){
+                            loop(handler);
+                        });
+                    }, 0);
+                }).catch(andFail);
+            }).catch(andFail);
+        }).catch(andFail);
+    }else{
+        fs.readdir(backupPath, function(err, list){
+            var files = list.filter(function(name){
+                return name[0] !== '.';
+            });
+            fs.readFile(path.join(backupPath, list[pos], function(err, body){
+                handler(body, function(){
+                    loop(handler);
+                });
+            }));
+        })
+        fs.readFile()
+    }
+}
+
+
+module.exports = {
+    pullAndProcessLoop: loop
+}
